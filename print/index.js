4a4ff820eb145a3c04e30b26067c3b2fdd13d466
sfilhu <sfilhu@gmail.com>
2020-04-09 16:09:36 +0000

    push first

--- print/index.js
+++ print/index.js
@@ -3,18 +3,19 @@ const SerialPort = require('serialport')
 
 const print = async (list) => {
     await SerialPort.list().then( async (ports) => {
-        const port = await ports.filter(item => item.manufacturer != undefined)
+        //const port = await ports.filter(item => item.manufacturer != undefined)
+        const printSerialPort = require('./print-serialport');
+        const { path } = ports[0];
         
-        if(port.length != 0){
-            const printSerialPort = require('./print-serialport');
-            const { path, comName } = port[0];
-            const osPort = process.platform === 'darwin' ? comName : path
-            await printSerialPort(list, osPort)
-        } else {
-            const printUSB = require('./print-usb');
-            await printUSB(list);
-            return true
-        }
+        await printSerialPort(list, path)
+        
+        // if(port.length != 0){
+            
+        // } else {
+        //     const printUSB = require('./print-usb');
+        //     await printUSB(list);
+        //     return true
+        // }
     })
 };
 
